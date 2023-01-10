7f5084fa52bbbd8d97869236cac86341711503a1
Neeraj Kumar <kneeraj0@gmail.com>
2023-01-10 15:19:17 +0530

    Initial Commit

---
+++ Template_SwiftUI/Template_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Template_SwiftUIApp.swift
+//  Template_SwiftUI
+//
+//  Created by Apple on 10/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Template_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
